27325a56227b42a9a49178d2b5777b4c22c5bc1f
CronNinja <richard@cronfusion.com>
2021-06-14 10:02:21 -0400

    login useEffect

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,15 @@
 import { withAuthenticator  } from '@aws-amplify/ui-react';
 import { useHistory } from 'react-router';
+import { Auth } from 'aws-amplify'
+import { useEffect } from 'react';
 const Login = ({userHasAuthenticated}) => {
    const history = useHistory();
-   userHasAuthenticated(true);
-   history.push("/");
+   useEffect(() => {
+    if(Auth.user){
+        userHasAuthenticated(true);
+        history.push("/");
+    }
+   }, [history, userHasAuthenticated])
     return (
         <div className="home">
         </div>
